80aa8c6f517f54636e4fe1ac9db866309fc8bcf0
James Allen <jrdallen97@gmail.com>
2019-12-11 18:35:08 +0000

    Problem 5: big speedup (~5.5 -> 0.45s)

--- problem_005.pl
+++ problem_005.pl
@@ -1,21 +1,23 @@
 #!/usr/bin/env perl
 use 5.028;
 
+my @check_factors = reverse 11..19;
+
 sub factors {
     my $num = shift;
 
-    for (reverse 11..20) {
+    for (@check_factors) {
         return 0 if $num % $_ != 0;
     }
 
     return 1;
 }
 
-my $i = 20;
+my $i = 220;
 while (1) {
     last if (factors($i) == 1);
 
-    $i += 20;
+    $i += 220;
 }
 
 say $i;
