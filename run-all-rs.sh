2d2cdff6077a25fea5655f82b7a5e5f80cbd5d81
James Allen <24575899+jrdallen97@users.noreply.github.com>
2024-05-14 10:28:48 +0100

    Add option to time scripts to silly runner thing

--- run-all-rs.sh
+++ run-all-rs.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+while getopts "t" arg; do
+  case $arg in
+    t) time_scripts=true;;
+  esac
+done
+
 # A stupid helper to run all my rust solutions in one go
 # Run `cargo build -r` first
 for f in target/release/p*; do
   if [[ "$f" =~ p[0-9]+$ ]]; then
     echo "$f"
-    eval "$f"
+    if [[ $time_scripts = true ]]; then
+      eval time "$f"
+    else
+      eval "$f"
+    fi
     echo
   fi
 done
